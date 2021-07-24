e74246ff86310aa2f8ca509ab134dfeeb65d5a4c
lwinmoehein <dev.lwinmoehein@gmail.com>
2021-07-24 15:08:30 +0630

    add:google drive and media fire support to link extractor

--- linkextractor.php
+++ linkextractor.php
@@ -12,19 +12,14 @@ $mediaFireLinkPattern = "/mediafire.com/i";
 $googleDriveLinkPattern = "/drive.google.com/i";
 
 $mediaFireLinkIdToBeScraped = "#downloadButton";
-$googleDriveLinkIdToBeScraped = "#uc-download-button";
-
-class Link {
-
-}
-
+$googleDriveLinkIdToBeScraped = "#uc-download-link";
 
 $mediaFire = 'https://www.mediafire.com/file/fa07cvryrx3lvel/The_Easiest_Way_To_Learn_Hiragana.mp4/file';
 $googleDrive =  'https://drive.google.com/u/0/uc?id=115Dxnbm0L0BX7t1qYh5JEAk4aAyOGwCN&export=download';
 
 
-$linkObject = new Link();
-$link->url = $mediaFire;
+$linkObject = new stdClass();
+$linkObject->url = $mediaFire;
 
 if(preg_match($mediaFireLinkPattern, $linkObject->url)){
     $linkObject->type = LINKTYPES::MEDIA_FIRE_LINK;
@@ -42,8 +37,8 @@ $crawler = $client->request('GET',$linkObject->url);
 switch($linkObject->type){
     case LINKTYPES::MEDIA_FIRE_LINK:
         $link = $crawler->filter($linkObject->linkPattern)->link();
-    default 
+    default :
         $link = $crawler->filter($linkObject->linkPattern)->link();
 }
-
+echo $linkObject->type."::";
 echo $link->getUri().PHP_EOL;
